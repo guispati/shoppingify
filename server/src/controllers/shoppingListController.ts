9d6e142b5256feaef84dddf931adba7dc225d9c2
Guilherme Spati <guilherme.spati723@gmail.com>
2022-11-18 21:29:45 -0300

    server: update status route

--- server/src/controllers/shoppingListController.ts
+++ server/src/controllers/shoppingListController.ts
@@ -57,4 +57,30 @@ export const getShoppingListById = catchAsync(async (req: CustomUserReq, res: Re
 });
 
 export const createShoppingList = factory.createOne(ShoppingList);
-export const updateShoppingListStatus = () => {};
+export const updateShoppingListStatus = catchAsync(async (req: CustomUserReq, res: Response, next: NextFunction) => {
+    const user = req.body.user;
+    const doc = await ShoppingList.findOneAndUpdate({
+        _id: req.params.id,
+        user: user,
+    }, {
+        $set: {
+            status: req.body.status,
+        }
+    }, {
+        new: true,
+        runValidators: true,
+    });
+
+    if (!doc) {
+        return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            data: doc,
+        },
+    });
+
+    next();
+});
